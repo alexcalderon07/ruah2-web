f9696784748321860662553c74e0de929b8f4642
alexcalderon07 <alexander.calderon240397@gmail.com>
2026-03-13 11:44:52 -0500

    test conexion

--- test.php
+++ test.php
@@ -1,1 +1,13 @@
-sv1
+<?php
+include 'config.php';
+
+$host = getenv('DB_HOST');
+$user = getenv('DB_USER');
+$password = getenv('DB_PASS');
+
+try {
+    $conn = new PDO("mysql:host=" . $host . ";dbname=srv1_account", $user, $password);
+    echo "✅ Conexión exitosa a srv1_account!";
+} catch(PDOException $e) {
+    echo "❌ Error de conexión: " . $e->getMessage();
+}
